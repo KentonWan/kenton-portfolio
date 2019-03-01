2566db736d6fd43e39f341dbf694b5bb3e8d71fe
Kenton Wan <kenton.wan@gmail.com>
2019-03-01 15:06:09 -0800

    refactored code to improve background

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -6,13 +6,16 @@ const aboutme = () => (
     <div className="main-container">
         
         <div className="container">
-            <p className="intro">Hi! My name is <span className="color">Kenton Wan</span>.</p> 
-            <img src={require("./Kenton_Profile_Pic.png")} height="200" alt="picture of Kenton" className="profilePic"/>
-            <div class="row">
-                <div className="col-md-6 offset-md-3">
-                    <p className="description text-justify">I am full stack web & software developer living in Los Angeles, CA.  I am passionate about learning new things, and writing clean and effective code.  I am a problem-solver that goes back to my days as a Doctor of Physical Therapy. I am drawn to the endless possibilities of things that can be built in development.</p>
+            <p className="intro">Hi! My name is <span className="color">Kenton Wan</span>.</p>
+            <div className="row">
+                <div className="col-md-4 offset-md-2 descriptionPic">
+                    <img src={require("./Kenton_Profile_Pic.png")} height="250" alt="picture of Kenton" className="profilePic"/>
                 </div>
-            </div>
+                <div className="col-md-3 descriptionDiv">
+                    <p className="description text-justify">I am full stack web and software developer living in Los Angeles, CA.  I am passionate about learning new things, and writing clean and effective code.  I am a problem-solver that goes back to my days as a Doctor of Physical Therapy. I am drawn to the endless possibilities of things that can be built in development.</p>
+                </div>            
+            </div> 
+           
         </div>
         <div className="container skills">
         <p className="skills"><span className="color">What I Do:</span>  JavaScript, HTML5, CSS3, React, NodeJS, APIs, Express, SQL, PostGreSQL, Git, Firebase, TDD, Jasmine </p>
--- src/components/landing.js
+++ src/components/landing.js
@@ -8,41 +8,69 @@ import Portfolio from "./portfolio.js";
 
 const landing = () => (
     <div>
-        <div className="row backgroundimg" id="home">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
             <div className="container">
-                <h1 className="name">Kenton Wan</h1>
-                <h4 className="subtitle">Full Stack Web Developer</h4>
-                <div>
-                    <a href="#about" id="About Me" className="links">About</a>
-                    <span>  |  </span>
-                    <a href="#portfolio" id="Portfolio" className="links">Portfolio</a>
+              <a className="navbar-brand" href="#home"><strong>KW</strong></a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent-7">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#home">Home <span className="sr-only">(current)</span></a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#about">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#portfolio">Portfolio</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="view" id="home">
+            <div className="mask rgba-black-light align-items-center">
+              <div className="container">
+                <div className="row">
+                  <div className="title col-md-12 mb-4 white-text text-center">
+                    <h1 className="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><strong>Kenton Wan</strong></h1>
+                    <hr className="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s"></hr>
+                    <h5 className="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Full Stack Web Developer</strong></h5>
+                  </div>
                 </div>
-
+              </div>
             </div>
-        </div>
-        <div id="about">
-            <AboutMe />
-            <div>
-                    <a href="#home" id="Home" className="links">Home</a>
-                    <span>  |  </span>
-                    <a href="#about" id="About Me" className="links">About</a>
-                    <span>  |  </span>
-                    <a href="#portfolio" id="Portfolio" className="links">Portfolio</a>
+          </div>
+
+        </header>
+
+        <main>
+          <div className="container aboutMe" id="about">
+            <div className="row py-5">
+              <div className="col-md-12 text-center">
+
+              <AboutMe />
+
+              </div>
             </div>
+          </div>
+        </main>
+        
+        <div className="portfolio" id="portfolio">
+            <div className="mask rgba-black-light align-items-center">
+              <div className="container">
+                <div className="row">
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
 
-        <div id="portfolio">
-            <Portfolio />
-            <div>
-                    <a href="#home" id="Home" className="links">Home</a>
-                    <span>  |  </span>
-                    <a href="#about" id="About Me" className="links">About</a>
-                    <span>  |  </span>
-                    <a href="#portfolio" id="Portfolio" className="links">Portfolio</a>
-            </div>
-        </div>
 
-    </div>
     
 );
 
